492e6dd06510f9f8df078d6acb829892d01f6903
spenpeq <spencer.peq@gmail.com>
2020-12-24 17:32:43 -0600

    Added: Updating/Tweaking models, Rendering data from database to template on dashboard

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -6,4 +6,5 @@ from .models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -14,6 +14,12 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     CATEGORY = (
@@ -25,8 +31,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Order(models.Model):
@@ -38,7 +49,9 @@ class Order(models.Model):
     
     
     
-    # customer = 
-    # product = 
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
+
