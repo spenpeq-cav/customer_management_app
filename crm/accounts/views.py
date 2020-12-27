57a70a9c5a85f14f18673a187b4c9dc0497d2253
spenpeq <spencer.peq@gmail.com>
2020-12-27 11:26:19 -0600

    Added multiple order placing for each customer using Inline Formsets

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 # Create your views here
 from .models import *
 from .forms import OrderForm
@@ -31,16 +32,20 @@ def customer(request, pk):
 
     return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    #form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
         # print('Printing post:', request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 
