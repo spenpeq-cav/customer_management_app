4f14e36d736b7856a9b9515c6d0e5f00ec871ab6
spenpeq <spencer.peq@gmail.com>
2020-12-29 12:25:02 -0600

    Added signals to create customer profiles. Moved logic from views.py registration method to signals.

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -31,9 +31,7 @@ def registerPage(request):
             user = form.save()
             username = form.cleaned_data.get('username')
 
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user,)
+            
             messages.success(request, 'Account was created for ' + username)
 
             return redirect('login')
--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django_filters',
 ]
 
