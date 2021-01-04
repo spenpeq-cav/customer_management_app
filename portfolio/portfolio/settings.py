4e911a5c0b1259a88e13ba9ca500d298199b6e43
spenpeq <spencer.peq@gmail.com>
2021-01-04 17:02:44 -0600

    Fix

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from secret_settings.py import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9m3dss8lc@&7=nk_#$+1x@962ke$liv6@8p*p_kh5p#nfu_dxp'
+
+# KET IN secret_settings
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
