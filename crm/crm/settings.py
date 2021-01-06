04c486ab83ec7a43871504253d7672a74022b0a9
Spencer Pequegnat <69612882+spenpeq@users.noreply.github.com>
2021-01-06 16:11:42 -0600

    Update settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = [
-    BASE_DIR / 'staticfiles',
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
