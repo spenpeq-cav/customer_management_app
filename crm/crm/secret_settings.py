cae97b22113a262e1cf6784ce32cab67d066f9eb
spenpeq <spencer.peq@gmail.com>
2021-01-05 21:27:06 -0600

    AWS modifications

---
+++ crm/crm/secret_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = '6@pe28oo7*(r&et6w^2y=dk3i-et&5rb$p6%g=8r7y2facbg=s'
+
+EMAIL_HOST_PASSWORD = 'django_test_pass'
+
+
+AWS_ACCESS_KEY_ID = 'AKIAXTPDUE3KBVR6VV6C'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'spencer-crm'
