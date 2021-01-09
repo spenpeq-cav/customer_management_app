1fe811e70f631ff82d93dc410a50e3f69f21321b
Spencer Pequegnat <69612882+spenpeq@users.noreply.github.com>
2021-01-08 21:40:18 -0800

    Delete secret_settings.py

--- crm/crm/secret_settings.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = '6@pe28oo7*(r&et6w^2y=dk3i-et&5rb$p6%g=8r7y2facbg=s'
-
-EMAIL_HOST_PASSWORD = 'django_test_pass'
-
-
-AWS_ACCESS_KEY_ID = 'AKIAXTPDUE3KD57IIEFE'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'spencer-crm'
